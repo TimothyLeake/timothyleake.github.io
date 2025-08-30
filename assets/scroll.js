5ea5ee393c3a6d172e0502440c9fa617807e421d
TimothyLeake <timothyleake@outlook.com>
2025-08-30 17:32:55 -0400

    Update scroll.js

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,5 +1,11 @@
 fetch("https://timothyleake.github.io/assets/binary.txt")
   .then(response => response.text())
   .then(data => {
-    document.getElementById("scroll").setAttribute("data-text", data);
+		let scrollState = data;
+		function scroll(){
+			scrollState = scrollState.slice(1) + scrollState[0];
+					document.getElementById("scroll").textContent = scrollState;
+		}
+		setInterval(scroll, 100);
+		};
   });

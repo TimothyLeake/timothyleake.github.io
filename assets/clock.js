d6b50002e6a281ff0d4c69e6ad0297be55541123
TimothyLeake <timothyleake@outlook.com>
2025-08-30 18:08:26 -0400

    Update clock.js

--- assets/clock.js
+++ assets/clock.js
@@ -1,1 +1,11 @@
-Bloop
+function updateClock() {
+	const today = new Date();
+	let h = today.getHours();
+	let m = today.getMinutes();
+	let s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+	setTimeout(updateClock, 1000);
+};
+window.onload = updateClock;
